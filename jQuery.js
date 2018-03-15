ff75fa125ca6a851616037e0c10a218eba26525b
SFPmedia <s-pauck@hotmail.com>
2018-03-15 22:21:07 +0100

    Mindre rettelser

---
+++ jQuery.js
@@ -0,0 +1,21 @@
+$(document).ready(() => {
+
+
+  $(".play-knap").click(function(){
+    $("#UHvideo").get(0).play();
+    $(this).hide();
+    $(".pause-knap").fadeIn(500);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+}
