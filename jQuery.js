e44f08eb78efa5f40bd8a2a1b70beebbb9867003
SFPmedia <s-pauck@hotmail.com>
2018-03-15 23:08:48 +0100

    jQuery færdigt. Mange ændringer på hjertene på index-siden!

--- jQuery.js
+++ jQuery.js
@@ -1,10 +1,60 @@
 $(document).ready(() => {
 
 
-  $(".play-knap").click(function(){
-    $("#UHvideo").get(0).play();
+
+  $(".heart1").click(function(){
+    $(".heart11").show();
     $(this).hide();
-    $(".pause-knap").fadeIn(500);
+  });
+
+  $(".heart2").click(function(){
+    $(".heart22").show();
+    $(this).hide();
+  });
+
+  $(".heart3").click(function(){
+    $(".heart33").show();
+    $(this).hide();
+  });
+
+  $(".heart4").click(function(){
+    $(".heart44").show();
+    $(this).hide();
+  });
+
+  $(".heart5").click(function(){
+    $(".heart55").show();
+    $(this).hide();
+  });
+
+
+
+
+  $(".heart11").click(function(){
+    $(".heart1").show();
+    $(this).hide();
+  });
+
+  $(".heart22").click(function(){
+    $(".heart2").show();
+    $(this).hide();
+  });
+
+  $(".heart33").click(function(){
+    $(".heart3").show();
+    $(this).hide();
+  });
+
+
+  $(".heart44").click(function(){
+    $(".heart4").show();
+    $(this).hide();
+  });
+
+
+  $(".heart55").click(function(){
+    $(".heart5").show();
+    $(this).hide();
   });
 
 
@@ -13,9 +63,4 @@ $(document).ready(() => {
 
 
 
-
-
-
-
-
-}
+});
